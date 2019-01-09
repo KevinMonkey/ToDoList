0144619dfa81e331b62f991710a6c1d82fb427cf
Kevin Nichols <kwnicho@asu.edu>
2019-01-08 19:14:51 -0700

    Convert to MVC design pattern.

---
+++ ToDoList/Data Model/Item.swift
@@ -0,0 +1,15 @@
+//
+//  Item.swift
+//  ToDoList
+//
+//  Created by Kevin Nichols on 1/8/19.
+//  Copyright © 2019 Kevin Nichols. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title: String = ""
+    var done: Bool = false
+    
+}
