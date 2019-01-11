6b9f5f685679ba0d7d2c66b737748d260a262ccd
Kevin Nichols <kwnicho@asu.edu>
2019-01-11 09:45:39 -0700

    Swipe Deletion working both the Category and Item controlled using a Super Class method.

--- ToDoList/Controllers/ToDoListViewController.swift
+++ ToDoList/Controllers/ToDoListViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import RealmSwift
 
-class ToDoListViewController: UITableViewController{
+class ToDoListViewController: SwipeTableViewController {
     
     var todoItems: Results<Item>?
     
@@ -23,6 +23,7 @@ class ToDoListViewController: UITableViewController{
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        loadItems()
         
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
@@ -34,16 +35,15 @@ class ToDoListViewController: UITableViewController{
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        let cell = super.tableView(tableView, cellForRowAt: indexPath)
         
         if let item = todoItems?[indexPath.row] {
+            cell.textLabel?.text = item.title
         
-        cell.textLabel?.text = item.title
+            // Ternary operator -->
+            // value = condiction ? valueIfTrue : valueIfFalse
         
-        // Ternary operator -->
-        // value = condiction ? valueIfTrue : valueIfFalse
-        
-        cell.accessoryType = item.done ? .checkmark : .none
+            cell.accessoryType = item.done ? .checkmark : .none
             
             } else {
             cell.textLabel?.text = "No Items Added Yet"
@@ -65,7 +65,6 @@ class ToDoListViewController: UITableViewController{
             } catch {
                 print("Error saving done status, \(error)")
             }
-            
         }
         
         tableView.reloadData()
@@ -119,7 +118,18 @@ class ToDoListViewController: UITableViewController{
         tableView.reloadData()
 
     }
-
+    
+    override func updateModel(at indexPath: IndexPath) {
+        if let item = todoItems?[indexPath.row] {
+            do {
+                try realm.write {
+                    realm.delete(item)
+                }
+            } catch {
+                print("Error deleting Item  ,\(error)")
+            }
+        }
+    }
 }
 
 //MARK - Search bar methods
